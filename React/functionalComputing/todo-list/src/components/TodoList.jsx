d169306430dcaa7c9b51a422fee42d08ecd9de0f
rolando-xyz <rolando@singularcnc.com>
2022-03-14 15:09:26 -0700

    todo list update

--- React/functionalComputing/todo-list/src/components/TodoList.jsx
+++ React/functionalComputing/todo-list/src/components/TodoList.jsx
@@ -6,9 +6,35 @@ const TodoList = () => {
 
     const handleNewTodoSubmit = (event) => {
         event.preventDefault();
-        setTodos([...todos, newTodo]);
+        if(newTodo.length == 0) {
+            return;
+        }
+        const todoItem = {
+            text: newTodo,
+            complete: false
+        };
+        setTodos([...todos, todoItem]);
         setNewTodo("");
     };
+
+    const handleTodoDelete = (delIdx) => {
+        const filteredTodos = todos.filter((todo,i) => {
+            return i != delIdx;
+        });
+        setTodos(filteredTodos);
+    }
+
+    const handleToggleComplete = (idx) => {
+        const updatedTodos = todos.map((todo,i) => {
+            if(idx == i) {
+                todo.complete = !todo.complete;
+                // const updatedTodo = {...todo, complete: !todo.complete};
+                // return updatedTodo;
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
+    }
 
   return (
     <div>
@@ -22,6 +48,20 @@ const TodoList = () => {
                 <button>Add</button>
             </div>
         </form>
+        <hr/>
+        {todos.map((todo, i) => {
+            return (
+                <div key={i}>
+                    <input onChange={(event) => {
+                        handleToggleComplete(i);
+                    }} checked={todo.complete} type="checkbox" />
+                    <span>{todo.text}</span>
+                    <button onClick={(event) =>{
+                        handleTodoDelete(i);
+                    }}>Delete</button>
+                </div>
+            )
+        })}
     </div>
   )
 }
