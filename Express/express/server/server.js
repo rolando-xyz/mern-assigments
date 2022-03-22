42d8afad1b40a6f144ffa614c519e2c3630d3d71
rolando-xyz <rolando@singularcnc.com>
2022-03-22 09:52:13 -0700

    algo commit

--- Express/express/server/server.js
+++ Express/express/server/server.js
@@ -2,5 +2,31 @@ const express = require("express")
 const app = express()
 const port = 8000
 
+const product = [
+
+];
+
+class Product {
+    constructor() {
+      this.name = faker.commerce.productName();
+      this.price = "$" + faker.commerce.price();
+      this.department = faker.commerce.department();
+    }
+  }
+      
+  console.log(new Product());
+  /*
+   * The output of the above console log will look like this
+   * {
+   *   name: 'Anime Figure',
+   *   price: '$568.00
+   *   department: 'Tools' 
+   * }
+   */
+  
+app.get("/api",(req,res)=>{
+    res.json("hello world")
+})
+
 // this needs to below the other code blocks
 app.listen( port, () => console.log(`Listening on port: ${port}`) );
