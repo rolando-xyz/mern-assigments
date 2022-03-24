ee8976fa1991f3a03c02b2154b2698a31f724e03
rolando-xyz <rolando@singularcnc.com>
2022-03-24 09:43:46 -0700

    algo commit

--- FullStackMern/productManager/client/src/components/Form.jsx
+++ FullStackMern/productManager/client/src/components/Form.jsx
@@ -7,13 +7,23 @@ const Form = () => {
     const [price, setPrice] = useState()
     const [description, setDescription] = useState("")
 
+    const handleSubmit = () => {
+        e.preventDefault()
+        axios.post("", {title, price, description})
+        .then(res=> history.push("/"))
+        .catch(err=> {
+            const errArr = err.response.data.errors
+            console.log(errArr)
+        })
+    }
+
   return (
     <fieldset>
         <legend>ProductManager.jsx</legend>
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label>Title</label>
-                <input type="text" name="title" onChange={setTitle} value="Title"/>
+                <input type="text" name="title" onChange={e=>setTitle.target.value} value={Title} className="form-control"/>
             </div>
             <div>
                 <label>Price</label>
