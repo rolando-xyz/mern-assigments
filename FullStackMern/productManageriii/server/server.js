3aafef5e4602b61d28ec7ac65b01fa1e7567cf23
rolando-xyz <rolando@singularcnc.com>
2022-03-25 09:46:15 -0700

    product manager 11

---
+++ FullStackMern/productManageriii/server/server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+
+require('./configs/mongoose.config');
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+require("./routes/product.routes")(app)
+app.listen(8000, () => console.log('listening on port: 8000'));
