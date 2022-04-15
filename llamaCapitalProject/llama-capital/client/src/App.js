c31c4a3ebd53d0f239c4d561c8b116457e39a692
Rolando Gomez <93492013+rolando-xyz@users.noreply.github.com>
2022-04-14 20:23:09 -0700

    llama capital frontend commit

--- llamaCapitalProject/llama-capital/client/src/App.js
+++ llamaCapitalProject/llama-capital/client/src/App.js
@@ -5,7 +5,6 @@ import CreateUser from './views/CreateUser';
 import UserLogin from './views/UserLogin';
 import Dashboard from './views/Dashboard';
 import Currency from './views/Currency';
-import Index from './views/Index';
 
 const BadLink = () => {
   return( <div className="container text-center text-danger mt-4"><h1 className="display-6">the ROUTE you attempted to travel was blocked by an unknown celestial event</h1></div> )
@@ -14,11 +13,7 @@ const BadLink = () => {
 function App() {
   return (
     <Routes>
-        <Route path='/' element={<Index />}>
-          <Route index element={<Main />} />
-          <Route path='*' element={<BadLink />} />
-        </Route>
-        <Route path='/user' element={<Navigation />}>
+        <Route path='/' element={<Navigation />}>
           <Route index element={<Main />} />
           <Route path='/user/new' element={<CreateUser />} />
           <Route path='/user/login' element={<UserLogin />} />
